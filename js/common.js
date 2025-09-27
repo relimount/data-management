b681d0bc861a375d38331f16cecc03395326f9f0
胡唐峰 <152974699+relimount@users.noreply.github.com>
2025-09-27 23:15:41 +0800

    抽取轻提示函数

--- js/common.js
+++ js/common.js
@@ -1,1 +1,11 @@
 axios.defaults.baseURL = 'https://hmajax.itheima.net'
+
+const showToast = (msg)=>{
+    const toastDom = document.querySelector('.my-toast')
+    const toast = new bootstrap.Toast(toastDom)
+
+    document.querySelector('.toast-body').innerHTML = msg
+    toast.show()
+}
+
+showToast('成功')
